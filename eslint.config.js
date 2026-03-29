afdf5e85617969cb387c214688b36cab9644d4ed
Mihajlovic, Marko <mihajlov39547@gmail.com>
2026-03-29 10:55:28 +0200

    Update ESLint configuration to disable additional TypeScript rules

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,17 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["supabase/functions/**/*.ts"],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
+      "no-empty": "off",
+      "no-control-regex": "off",
     },
   },
 );
